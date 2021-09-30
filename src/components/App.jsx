8c35849f249e81274790229a52b09df797c4f9d1
neethunsr <neethu2001nsr@gmail.com>
2021-09-30 20:19:23 +0530

    Organise component files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Notes from './Notes';
-import notes from './notes';
+import notes from '../notes';
 
 function App(){
     return <div>
