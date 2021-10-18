021f4dbc087a8b75b7f2fcf3627cafc3737f6358
neethunsr <neethu2001nsr@gmail.com>
2021-10-18 12:27:13 +0530

    Add and Delete notes option

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,12 @@ import Header from './Header';
 import Footer from './Footer';
 import Notes from './Notes';
 import notes from '../notes';
+import AddNote from './AddNote';
 
 function App(){
     return <div>
         <Header />
-
+        <AddNote />
         {notes.map(note =>
             <Notes 
                 key={note.key}
@@ -15,7 +16,6 @@ function App(){
                 content={note.content}
             />
         )}
-
         <Footer />
     </div>
 }
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function Notes(props) {
-    return <div className="note">
+    return( 
+    <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button>Delete</button>
     </div>
+    );
 }
 
 export default Notes;
